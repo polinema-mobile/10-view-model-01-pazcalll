144c687c3843d52b34da695699b8293f7a05043f
pazcalll <yazeedarifin@gmail.com>
2020-10-30 13:32:50 +0700

    done

--- app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
+++ app/src/main/java/id/putraprima/courtcounter/viewmodels/CounterViewModels.java
@@ -30,4 +30,11 @@ public class CounterViewModels extends ViewModel {
     }
 
     //TODO : Tambahkan Implementasi View Model Untuk Reset Point
+    public void resetAll(){
+        score.setAwayScore(0);
+        score.setHomeScore(0);
+        scoreMutableLiveData.setValue(score);
+        Log.d("Point Home : ", String.valueOf(score.getHomeScore()));
+        Log.d("Point Away : ", String.valueOf(score.getAwayScore()));
+    }
 }
